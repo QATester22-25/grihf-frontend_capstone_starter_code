1120485b2dabab06b6aa55750557790cc0ffa54b
QATester22-25 <hednaili@gmail.com>
2026-03-27 18:33:53 -0400

    logout

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,68 +1,101 @@
-import React from 'react'
-import "./Navbar.css";                       
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Navbar.css";
+
+
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
 
-    function handleClick() {
-        const navLinks = document.querySelector(".nav__links");
-        const navIcon = document.querySelector(".nav__icon i");
-  
-        
-        navLinks.classList.toggle("active");
-  
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const[email,setEmail]=useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // remove email phone
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        // setUsername("");
        
-        if (navLinks.classList.contains("active")) {
-          navIcon.classList.remove("fa-bars");
-          navIcon.classList.add("fa-times");
-        } else {
-          navIcon.classList.remove("fa-times");
-          navIcon.classList.add("fa-bars");
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          if (key.startsWith("reviewFormData_")) {
+            localStorage.removeItem(key);
+          }
         }
-      }
-      
-    return (
-        <>
-<nav>
-  <div class="nav__logo">
+        setEmail('');
+        window.location.reload();
+    }
+    const handleDropdown = () => {
+      setShowDropdown(!showDropdown);
+    }
+    useEffect(() => { 
+      const storedemail = sessionStorage.getItem("email");
 
-    <a href="/">
-      Stay Healthy
-    </a>
+      if (storedemail) {
+            setIsLoggedIn(true);
+            setUsername(storedemail);
+          }
+        }, []);
+  return (
+    <nav>
+      <div className="nav__logo">
+        <Link to="/">
+        StayHealthy <i style={{color:'#2190FF'}} className="fa fa-user-md"></i></Link>
+        <span>.</span>
+      </div>
+      <div className="nav__icon" onClick={handleClick}>
+        <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+      </div>
+      <ul className={click ? 'nav__links active' : 'nav__links'}>
+        <li className="link">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="link">
+          <Link to="/search/doctors">Appointments</Link>
+        </li>
+        <li className="link">
+          <Link to="/healthblog">Health Blog</Link>
+        </li>
+        <li className="link">
+         <Link to="/reviews">Reviews</Link>
+        </li>
+        {isLoggedIn?(
+          <>
+            <li className="link">
+              <button className="btn2" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+            
+          </>
+        ) : (
+          <>
+            <li className="link">
+              <Link to="/signup">
+                <button className="btn1">Sign Up</button>
+              </Link>
+            </li>
+            <li className="link">
+              <Link to="/login">
+                <button className="btn1">Login</button>
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
-  </div>
+export default Navbar;
 
-  <div class="nav__icon">
-
-    <i class="fa fa-times fa fa-bars"></i>
-  </div>
-
-
-  <ul class="nav__links active">
-
-    <li class="link">
-    <Link to="/">Home</Link>
-    </li>
-
-    <li class="link">
-    <Link to="/Appointments">Appointments</Link>
-    </li>
-
-    <li class="link">
-      <Link to="/SignUp">
-       SignUp
-     </Link>
-    </li>
-    <li class="link">
-      <Link to="/Login" >
-       Login
-      </Link>
-    </li>
-  </ul>
-</nav>
-</>
-    )
-  
-}
-
-export default Navbar
