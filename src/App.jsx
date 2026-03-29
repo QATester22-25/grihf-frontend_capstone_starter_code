a396e34b310bc35ed748e2bd56ab118094dbdd22
QATester22-25 <qa.tester22.25@gmail.com>
2026-03-29 19:00:34 +0100

    Add Instant consultation component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login.jsx';
 import SignUp from './Components/SignUp/SignUp.jsx';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultationBooking/InstantConsultation.jsx';
+import FindDoctorSearchIC from './Components/InstantConsultationBooking/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC.jsx';
+import AppointmentFormIC from './Components/InstantConsultationBooking/InstantConsultationBooking/AppointmentFormIC/AppointmentFormIC.jsx';
+import DoctorCardIC from './Components/InstantConsultationBooking/InstantConsultationBooking/DoctorCardIC/DoctorCardIC.jsx';
 
 function App() {
 
@@ -15,7 +19,9 @@ function App() {
             <Route path="/" element={<Landing_Page/>}/>
             <Route path="/Login" element={<Login/>}/>
             <Route path="/SignUp" element={<SignUp/>}/>
-            <Route path="/LandingPage" element={<Landing_Page/>}/>
+            <Route path="/LandingPage" element={<Landing_Page />} />
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="/search/doctors" element={<FindDoctorSearchIC />} />
         </Routes>
         </BrowserRouter>
     
